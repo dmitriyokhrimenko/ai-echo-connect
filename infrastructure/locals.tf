ff7fb16ee6585828ade2a210db7c65e396f31414
dmytrookhrymenko <dmitriy.okhrimenko@easternpeak.com>
2025-03-30 17:29:30 -0600

    feat(infra): Add AWS VPC creation

---
+++ infrastructure/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  env             = "staging"
+  region          = "ca-central-1"
+  zone1           = "ca-central-1a"
+  zone2           = "ca-central-1b"
+  eks_name        = "crypto-tracker-api"
+ // eks_version     = "1.32"
+  profile         = "crypto-tracker-api"
+}
