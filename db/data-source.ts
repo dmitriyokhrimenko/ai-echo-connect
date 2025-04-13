f2b2ccb976f95ffd1e0ce368d8d8e0a0a9de4dcd
dmytrookhrymenko <dmitriy.okhrimenko@easternpeak.com>
2025-04-13 00:25:34 -0600

    feat(db): add data-source

---
+++ db/data-source.ts
@@ -0,0 +1,32 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+config();
+
+const configService = new ConfigService();
+
+const isCompiled = __filename.endsWith('.js');
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: Number(configService.get<string>('DB_PORT')),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_DATABASE'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [
+    isCompiled ? 'dist/migrations/**/*.js' : 'db/migrations/**/*.ts',
+  ],
+  synchronize: false,
+  ...(configService.get<string>('ENV') === 'development' && {
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  }),
+};
+
+export const AppDataSource = new DataSource(dataSourceOptions);
