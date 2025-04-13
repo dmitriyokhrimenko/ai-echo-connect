ff35d65e09287c4111452448f3efe5e7f6003626
dmytrookhrymenko <dmitriy.okhrimenko@easternpeak.com>
2025-04-13 00:39:06 -0600

    feat(common): install i18n module

--- src/common/middlewares/locale.middleware.ts
+++ src/common/middlewares/locale.middleware.ts
@@ -1,9 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
+interface AppRequest extends Request {
+  locale?: string;
+}
+
 @Injectable()
 export class LocaleMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  use(req: AppRequest, res: Response, next: NextFunction) {
     req.locale = req.headers['accept-language']?.split(',')[0] || 'en';
     next();
   }
